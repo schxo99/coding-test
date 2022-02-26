9e14239fd7618ab06de15c23220ec7cba9cbc9b9
schxo99 <94772918+schxo99@users.noreply.github.com>
2022-02-26 22:24:08 +0900

    Add files via upload

---
+++ 프로그래머스/레벨 1/숫자 문자열과 영단어.py
@@ -0,0 +1,16 @@
+s = "one4seveneight"	
+def solution(s):
+    # s = s.replace('one', '1').replace('two', '2').replace('three', '3').replace('four', '4').replace('five', '5').replace('six', '6').replace('seven', '7').replace('eight', '8').replace('nine', '9').replace('zero', '0')
+    s = s.replace('one', '1')
+    s = s.replace('two', '2')
+    s = s.replace('three', '3')
+    s = s.replace('four', '4')
+    s = s.replace('five', '5')
+    s = s.replace('six', '6')
+    s = s.replace('seven', '7')
+    s = s.replace('eight', '8')
+    s = s.replace('nine', '9')
+    s = s.replace('zero', '0')
+    print(s)
+    # return result
+solution(s)
