c229828b158c50625ac09740b1164f31a0e214be
schxo99 <94772918+schxo99@users.noreply.github.com>
2022-11-11 12:44:58 +0900

    Add files via upload

---
+++ 프로그래머스/레벨 2/주차요금계산.py
@@ -0,0 +1,23 @@
+import math
+def solution(fees, records):
+    answer = []
+    inTime = [0]*10000
+    isIn = [0]*10000
+    sumT = [0]*10000
+    for record in records:
+        a,b,c = record.split()
+        h,m = a.split(":")
+        if c == "IN":
+            inTime[int(b)] = int(h)*60+int(m)
+            isIn[int(b)] = 1
+        else:
+            sumT[int(b)]+=(int(h)*60+int(m) - inTime[int(b)])
+            isIn[int(b)] = 0
+    for i in range(10000):
+        if isIn[i] == 1:
+            sumT[i] += (23*60+59) - inTime[i]
+            
+    for i in range(10000):
+        if sumT[i]>0:
+            answer.append(fees[1]+max(math.ceil((sumT[i]-fees[0])/fees[2]), 0)*fees[3])
+    return answer
