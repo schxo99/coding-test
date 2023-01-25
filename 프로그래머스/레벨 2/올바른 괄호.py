4d81443512265ef40031e914db488b15d3fad917
schxo99 <94772918+schxo99@users.noreply.github.com>
2023-01-25 15:38:32 +0900

    Update 올바른 괄호.py

--- 프로그래머스/레벨 2/올바른 괄호.py
+++ 프로그래머스/레벨 2/올바른 괄호.py
@@ -1,11 +1,58 @@
+#4차 성공!
+#pop보단 슬라이싱하는게 더 효율적인듯.
+#replace 함수보다 스택 활용하는게 더 효율적인듯
 def solution(s):
+    s = list(s)
+    arr = []
     for i in range(len(s)):
-        a = s.replace('()', '')
-        if a == s:
-            break
-        else:
-            s = a
-    if len(s) >= 1:
+        arr.append(s[i])
+
+        if len(arr) >=2 and arr[-1] == ')' and arr[-2] == '(':
+            del arr[-2:]
+    if len(arr) >= 1:
         return False
     else:
-        return True
+         return True
+
+#3차 시간초과 하나만 오류
+# def solution(s):
+#     s = list(s)
+#     arr = []
+#     for i in range(len(s)):
+#         arr.append(s[i])
+
+#         if len(arr) >=2 and arr[-1] == ')' and arr[-2] == '(':
+#             arr.pop()
+#             arr.pop()
+#     if len(arr) >= 1:
+#         return False
+#     else:
+#          return True
+        
+#2차 시간초과 오류
+# def solution(s):
+#     s = list(s)
+#     arr = []
+#     for i in range(len(s)):
+#         arr.append(s[0])
+#         del s[0]
+#         if len(arr) >=2 and arr[-1] == ')' and arr[-2] == '(':
+#             arr.pop()
+#             arr.pop()
+#     if len(arr) >= 1:
+#         return False
+#     else:
+#          return True
+        
+#1차 시간초과 오류
+# def solution(s):
+#     for i in range(len(s)//2):
+#         a = s.replace('()', '')
+#         if a == s:
+#             break
+#         else:
+#             s = a
+#     if len(s) >= 1:
+#         return False
+#     else:
+#         return True
