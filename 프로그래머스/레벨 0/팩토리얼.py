7ed71423f97f570ea7a44af9725a7316e1dfb96f
schxo99 <94772918+schxo99@users.noreply.github.com>
2023-02-09 13:23:27 +0900

    Update 팩토리얼.py

--- 프로그래머스/레벨 0/팩토리얼.py
+++ 프로그래머스/레벨 0/팩토리얼.py
@@ -1,1 +1,12 @@
-
+def solution(n):
+    a = 1
+    p = 1
+    while True:
+        if p == n:
+            break
+        elif p >= n:
+            a-=1
+            break
+        a+=1
+        p *= a
+    return a
