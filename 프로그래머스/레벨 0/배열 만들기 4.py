5769ef1b4f925d4a84657bfb71ef755b496d3617
ChanHwi Shin <94772918+schxo99@users.noreply.github.com>
2023-06-21 15:48:31 +0900

    Create 배열 만들기 4.py

---
+++ 프로그래머스/레벨 0/배열 만들기 4.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    i = 0
+    stk = []
+    while i < len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i+=1
+        if stk and stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i+=1
+        elif stk and stk[-1] >= arr[i]:
+            stk.pop()
+    return stk
